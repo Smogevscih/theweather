00d681984833981653f4649d1450f5936751e878
Yuriy <57864435+Smogevscih@users.noreply.github.com>
2020-07-08 07:07:55 +0500

    add new interface ContractOne for MVP

---
+++ app/src/main/java/com/smic/weather/contracts/ContractOne.java
@@ -0,0 +1,22 @@
+package com.smic.weather.contracts;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * @autor Smogevscih Yuri
+ * 08.07.2020
+ **/
+public interface ContractOne {
+    interface BModel {
+
+    }
+
+    interface Presenter {
+
+
+    }
+
+    interface View {
+
+    }
+}
