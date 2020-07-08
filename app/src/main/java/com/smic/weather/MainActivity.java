5e144e447c195879b5f966af171f41bca684de86
Yuriy <57864435+Smogevscih@users.noreply.github.com>
2020-07-08 09:08:45 +0500

    1. modify name buttons in activity_main.xml. 2. add listener for buttons

--- app/src/main/java/com/smic/weather/MainActivity.java
+++ app/src/main/java/com/smic/weather/MainActivity.java
@@ -3,6 +3,7 @@ package com.smic.weather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
@@ -11,30 +12,50 @@ import android.widget.TextView;
 import com.smic.weather.contracts.ContractOne;
 import com.smic.weather.presenters.PresenterOne;
 
-public class MainActivity extends AppCompatActivity implements ContractOne.View {
-    private Button button, button2, button3;
+public class MainActivity extends AppCompatActivity implements ContractOne.View, View.OnClickListener {
+    private Button btnConnectBD, btnEditCity, btnGetListFromBD;
     private Spinner spinner1, spinner2, spnModeScale;
     private TextView txtTypeCity, txtAnswer;
     PresenterOne presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        presenter=new PresenterOne(this);
+        presenter = new PresenterOne(this);
         {
-            button = findViewById(R.id.button);
-            button2 = findViewById(R.id.button2);
-            button3 = findViewById(R.id.button3);
+            btnConnectBD = findViewById(R.id.btnConnectBD);
+            btnEditCity = findViewById(R.id.btnEditCity);
+            btnGetListFromBD = findViewById(R.id.btnGetListFromBD);
             spinner1 = findViewById(R.id.spinner1);
             spinner2 = findViewById(R.id.spinner2);
             spnModeScale = findViewById(R.id.spnModeScale);
             txtTypeCity = findViewById(R.id.txtTypeCity);
             txtAnswer = findViewById(R.id.txtAnswer);
         }
+        btnConnectBD.setOnClickListener(this);
+        btnEditCity.setOnClickListener(this);
+        btnGetListFromBD.setOnClickListener(this);
     }
 
     @Override
     public void showSpinnerCity(ArrayAdapter adapterCity) {
         spinner1.setAdapter(adapterCity);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnConnectBD:
+
+                break;
+            case R.id.btnEditCity:
+
+
+                break;
+            case R.id.btnGetListFromBD:
+
+                break;
+        }
     }
 }
