4441be8c3b5998dc9691fb4e34cbce534ea7eff8
Yuriy <57864435+Smogevscih@users.noreply.github.com>
2020-07-08 07:11:57 +0500

    add new classes TemperatureInCity - BModel, PresenterOne - presenter

---
+++ app/src/main/java/com/smic/weather/bmodel/TemperatureInCity.java
@@ -0,0 +1,11 @@
+package com.smic.weather.bmodel;
+
+import com.smic.weather.contracts.ContractOne;
+
+/**
+ * @autor Smogevscih Yuri
+ * 08.07.2020
+ **/
+public class TemperatureInCity implements ContractOne.BModel{
+
+}
---
+++ app/src/main/java/com/smic/weather/presenters/PresenterOne.java
@@ -0,0 +1,11 @@
+package com.smic.weather.presenters;
+
+import com.smic.weather.contracts.ContractOne;
+
+/**
+ * @autor Smogevscih Yuri
+ * 08.07.2020
+ **/
+class PresenterOne implements ContractOne.Presenter{
+
+}
